457b39c8d1fe535e0bffec337a082fa562c9c0c6
bensonitguy <bensonitguy@gmail.com>
2019-03-22 12:12:37 +0530

    added Video Item component and added vvideo thumbnail and unique key

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = (props) => {
-    return <div>{props.videos.length}</div>;
+
+const VideoList = ({videos}) => { // es 2016 code to get videos from props.videos
+    
+    const renderedList = videos.map((video) => {
+        return <VideoItem key={video.id.videoId} video={video}/>
+    });
+
+    return <div>{renderedList}</div>
 };
 
 export default VideoList;
